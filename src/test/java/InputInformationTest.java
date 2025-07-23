cba9b37fb35d3702ed4dded232401b073f17edcc
Rokinol <rokino97@gmail.com>
2025-07-23 15:44:07 +0300

    corrections to lesson_9

--- src/test/java/InputInformationTest.java
+++ src/test/java/InputInformationTest.java
@@ -4,6 +4,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 import java.time.Duration;
 
 public class InputInformationTest extends BaseTest {
@@ -24,6 +26,11 @@ public class InputInformationTest extends BaseTest {
 
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(1));
+
+        WebElement payButton = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//button[contains(text(), 'Оплатить')]")));
+        assertTrue(payButton.isDisplayed(), "Кнопка 'Оплатить' не отображается");
+        assertEquals("Оплатить 10.00 BYN", payButton.getText().trim(), "Текст кнопки не совпадает");
 
         driver.findElement(By.xpath("//button[contains(text(), 'Оплатить')]")).isDisplayed();
     }
