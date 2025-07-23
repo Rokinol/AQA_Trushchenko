c92ca5e309430bcd10a8999320aaace3b6260f6e
Rokinol <rokino97@gmail.com>
2025-07-23 17:55:09 +0300

    corrections to lesson_9

--- src/test/java/PayWrapperTest.java
+++ src/test/java/PayWrapperTest.java
@@ -1,11 +1,18 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PayWrapperTest extends BaseTest {
 
     @Test
     void payWrapperTest() {
 
-            driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение')]")).isDisplayed();
+        WebElement element = driver.findElement(By.xpath("//h2[contains(text(), 'Онлайн пополнение')]"));
+        assertAll(
+                () -> assertTrue(element.isDisplayed(), "Элемент не отображается на странице"),
+                () -> assertEquals("Онлайн пополнение\nбез комиссии", element.getText().trim(), "Текст элемента не совпадает")
+        );
     }
 }
